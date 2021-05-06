1ec98441d8d75da92b7606f89103874344b33382
supercop185 <supercop185@gmail.com>
2021-05-07 00:49:22 +0100

    sensors e limites

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var readlineSync = require('readline-sync');
 let fs = require('fs');
+const { Console } = require('console');
 
 // Declaração de matrizes
 
@@ -34,7 +35,7 @@ function Init_Matriz_Map() {
         for (j = 0; j < 5; j++) {
             Matriz_Mapeamento[i][j] = 0;
         }
-        console.log(Matriz_Mapeamento[i]);
+        //console.log(Matriz_Mapeamento[i]);
     }
 }
 
@@ -47,7 +48,7 @@ function Init_Matriz_Map_ASV() {
             Matriz_ASV[i][j] = 'O';
         }
     }
-    console.table(Matriz_ASV);
+    //console.table(Matriz_ASV);
 }
 
 //Inicia a matriz da area de funcionamento do ASV 
@@ -64,7 +65,7 @@ function Init_Matriz_Coord() {
             if (j != 0) {
                 y -= 0.000020;
             }
-        M_Coord[i][j] = { asv_Lat: x.toFixed(6), asv_Long: y.toFixed(6) };
+            M_Coord[i][j] = { asv_Lat: x.toFixed(6), asv_Long: y.toFixed(6) };
         }
         x += 0.000005;
     }
@@ -76,53 +77,184 @@ function Init_Matriz_Coord() {
 
 //const nome = require("./ASV_Coord.json");
 
-function Ler_Ficheiro_Init_Matriz_Coord(){
+function Ler_Ficheiro_Init_Matriz_Coord() {
     let i, j;
 
     M_Coord = JSON.parse(fs.readFileSync('./ASV_Coord.json', 'utf8'));
     console.table(M_Coord);
-/* 
-    for (i = 0; i < 11; i++) 
-        for (j = 0; j < 5; j++) 
-            console.log(M_Coord[i][j]); */
+    /* 
+        for (i = 0; i < 11; i++) 
+            for (j = 0; j < 5; j++) 
+                console.log(M_Coord[i][j]); */
 }
 
 function ASV_Moves(i, j) {
 
     let x = 0, y = 0;
+    let Vet_ASV_Dir = [024, 025, 026, 027]
 
     if ((i == 1) && (j == 0)) {
         Matriz_ASV[0][y] = '*';//esta pode-se tirar
-        Vet_ASV_Seq_Pos.push({CoordX : 0, CoordY : j});//esta pode-se tirar
+        Vet_ASV_Seq_Pos.push({ CoordX: 0, CoordY: j });//esta pode-se tirar
         Matriz_ASV[i][j] = '*';
-        Vet_ASV_Seq_Pos.push({CoordX : i, CoordY : j});
+        Vet_ASV_Seq_Pos.push({ CoordX: i, CoordY: j });
     }
     else if (Math.abs(Vet_ASV_Pos[0] - i) == 0) {
         Matriz_ASV[i][j] = '*';
-        Vet_ASV_Seq_Pos.push({CoordX : i, CoordY : j});
+        Vet_ASV_Seq_Pos.push({ CoordX: i, CoordY: j });
     }
     else if (Math.abs(Vet_ASV_Pos[0] - i) > 1) {
         for (x = Vet_ASV_Pos[0] + 1; x <= i; x++) {
             Matriz_ASV[x][j] = '*';
-            Vet_ASV_Seq_Pos.push({CoordX : i, CoordY : j});
+            Vet_ASV_Seq_Pos.push({ CoordX: i, CoordY: j });
         }
     }
     Vet_ASV_Pos[0] = i;
     Vet_ASV_Pos[1] = j;
-    console.table(Matriz_ASV);
-    console.table(Vet_ASV_Seq_Pos[0]);
+    /*     var ss = String.fromCharCode(Vet_ASV_Dir[0]);
+        console.log("sentido", ss); */
+    //console.table(Vet_ASV_Seq_Pos[0]);
 }
 
-function fill(i, j){
+//função experimental preenchimento obstaculos
+//Com Head´s
+//O Retorno dos J tem de se ler invertido
+function direction(i, j, head) {
+
+    let auxIJ = [];
+
+    if (head == 'R' || head == 'L') {
+        if (i - 1 >= 0)
+            auxIJ[0] = i - 1;
+        else
+            auxIJ[0] = i;
+        auxIJ[1] = i;
+        if (i + 1 <= Matriz_Mapeamento.length)
+            auxIJ[2] = i + 1;
+        else
+            auxIJ[2] = i;
+        if (head == 'R') {
+            if (j + 1 <= Matriz_Mapeamento[i].length)
+                auxIJ[3] = j + 1;
+            else
+                auxIJ[3] = j;
+        }
+        else {
+            if (j - 1 >= 0)
+                auxIJ[3] = j - 1;
+            else
+                auxIJ[3] = j;
+
+        }
+        return (auxIJ);
+    }
+    else {
+        if (j - 1 >= 0)
+            auxIJ[0] = j - 1;
+        else
+            auxIJ[0] = j;
+        auxIJ[1] = j;
+        if (j + 1 <= Matriz_Mapeamento[i].length)
+            auxIJ[2] = j + 1;
+        else
+            auxIJ[2] = j;
+        if (head == 'D') {
+            if (i + 1 <= Matriz_Mapeamento.length)
+                auxIJ[3] = i + 1;
+            else
+                auxIJ[3] = i;
+        }
+        else {
+            if (i - 1 >= 0)
+                auxIJ[3] = i - 1;
+            else
+                auxIJ[3] = i;
+        }
+        return (auxIJ);
+    }
+
+}
+
+//Função que controla o preenchimento com os obstaculos encontrados
+function fill_Obst(auxIJ, head, obst_d, obst_m, obst_u) {
+
+    //let aux_x, aux_y, xi, yi, xf, yf;
+    //console.log(auxIJ, head, obst_d, obst_m, obst_u);
+    if (head == 'R' || head == 'L') {
+        if (obst_m == 2) {
+            if (Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] == 3)
+                Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] = 4;
+            else if (Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] == 0)
+                Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] = obst_m;
+        }
+        else {
+            if (Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] == 0)
+                Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] = 1;
+        }
+        if (obst_u == 2) {
+            if (Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] == 3)
+                Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] = 4;
+            else if (Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] == 0)
+                Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] = obst_u;
+        }
+        else {
+            if (Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] == 0)
+                Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] = 1;
+        }
+        if (obst_d == 2) {
+            if (Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] == 3)
+                Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] = 4;
+            else if (Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] == 0)
+                Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] = obst_d;
+        }
+        else {
+            if (Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] == 0)
+                Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] = 1;
+        }
+    }
+    else {
+        if (obst_m == 2) {
+            if (Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] == 3)
+                Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] = 4;
+            else if (Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] == 0)
+                Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] = obst_m;
+        }
+        else if (Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] == 0) {
+            Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] = 1;
+        }
+        if (obst_u == 2) {
+            if (Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] == 3)
+                Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] = 4;
+            else if (Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] == 0)
+                Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] = obst_u;
+        }
+        else if (Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] == 0) {
+            Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] = 1;
+        }
+        if (obst_d == 2) {
+            if (Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] == 3)
+                Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] = 4;
+            else if (Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] == 0)
+                Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] = obst_d;
+        }
+        else if (Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] == 0) {
+            Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] = 1;
+        }
+    }
+    //console.table(Matriz_Mapeamento);
+}
+
+//Função que controla o preenchimento com a poluição encontrada
+function fill_Pol(i, j, pol1) {
     let aux_x, aux_y, xi, yi, xf, yf;
 
-    console.log(Matriz_Mapeamento.length,Matriz_Mapeamento[i].length)
+    //console.log(Matriz_Mapeamento.length, Matriz_Mapeamento[i].length)
     //verificar coordenadas iniciais controlando as linhas i
-    if (i == 0){
+    if (i == 0) {
         xi = i;
         xf = i + 1;
     }
-    else{
+    else {
         xi = i - 1;
         if ((i + 1) > Matriz_Mapeamento.length)
             xf = i;
@@ -130,92 +262,146 @@ function fill(i, j){
             xf = i + 1;
     }
     //verificar coordenadas iniciais controlando as colunas j
-    if (j == 0){
+    if (j == 0) {
         yi = j;
         yf = j + 1;
     }
-    else{
+    else {
         yi = j - 1;
         if ((j + 1) > Matriz_Mapeamento[i].length)
             yf = j;
         else
             yf = j + 1;
     }
+    console.log("i ", "j ", "xi ", "yi ", "xf ", "yf ");
     console.log(i, j, xi, yi, xf, yf);
     for (aux_x = xi; aux_x <= xf; aux_x++)
         for (aux_y = yi; aux_y <= yf; aux_y++)
-            Matriz_Mapeamento[aux_x][aux_y] = 3;
-    console.table(Matriz_Mapeamento);
+            if (pol1 != 3)
+                Matriz_Mapeamento[i][j] = 1;
+            else
+                Matriz_Mapeamento[aux_x][aux_y] = pol1;
+
+    //console.table(Matriz_Mapeamento);
+}
+
+function sensors(i, j, head) {
+    console.log("leitura :", i, j, head, Matriz_Mapeamento[i][j]);
+    if (Matriz_Mapeamento[i][j] != 2 && Matriz_Mapeamento[i][j] != 3 && Matriz_Mapeamento[i][j] != 4) {
+        poluicao = readlineSync.question('Sensor poluição 0 ou 3 ? ');
+        fill_Pol(i, j, poluicao);
+    } 
+    auxIJ = direction(i, j, head);
+    console.log(auxIJ, head);
+    if (!(i == Matriz_Mapeamento.length && head == 'D') && !(i == 0 && head == 'U') && 
+            !(j == 0 && head == 'L') && !(j + 1 == Matriz_Mapeamento[i].length && head == 'R')) {
+        console.log("entrei");
+        if (head == 'R' || head == 'L'){
+            if (Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] == 0 || Matriz_Mapeamento[auxIJ[1]][auxIJ[3]] == 3)
+                obst_m = readlineSync.question('Sensor meio obstaculo 0 ou 2 ? ');
+            if (Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] == 0 || Matriz_Mapeamento[auxIJ[0]][auxIJ[3]] == 3)
+                obst_u = readlineSync.question('Sensor cima obstaculo 0 ou 2 ? ');
+            if (Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] == 0 || Matriz_Mapeamento[auxIJ[2]][auxIJ[3]] == 3)
+                obst_d = readlineSync.question('Sensor baixo obstaculo 0 ou 2 ? ');
+        }
+        else{
+            if (Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] == 0 || Matriz_Mapeamento[auxIJ[3]][auxIJ[1]] == 3)
+                obst_m = readlineSync.question('Sensor meio obstaculo 0 ou 2 ? ');
+            if (Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] == 0 || Matriz_Mapeamento[auxIJ[3]][auxIJ[0]] == 3)
+                obst_u = readlineSync.question('Sensor cima obstaculo 0 ou 2 ? ');
+            if (Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] == 0 || Matriz_Mapeamento[auxIJ[3]][auxIJ[2]] == 3)
+                obst_d = readlineSync.question('Sensor baixo obstaculo 0 ou 2 ? ');
+        }
+        //auxIJ = direction(i, j, head);
+        console.log(auxIJ, head, obst_d, obst_m, obst_u);
+        fill_Obst(auxIJ, head, obst_d, obst_m, obst_u);
+    }
 }
 
 // 1 - Viu, 2 - Bloco, 3 - Poluida, 4 -- ambos
 function ASV_Mapping() {
+    let auxIJ = [];
     let opt1 = true;
-    let opt = 1, poluicao, obst, obst1, obst2;
-    let i = 1, j = 0, flag = 0;
+    let opt = 1, poluicao, obst_m, obst_u, obst_d;
+    let i = 1, j = 0, aux_i, aux_j, flag = 0, flag_h = 0; head = 'R';
 
+    ASV_Moves(i, j);
+    sensors(i, j, head);
+    console.table(Matriz_ASV);
+    console.table(Matriz_Mapeamento);
     while (opt1) {
-        poluicao = readlineSync.question('Sensor poluição 0 ou 3 ? ');//ver o que falta
-        if (poluicao == 3){
-            fill(i, j);
-            obst = readlineSync.question('Sensor meio obstaculo 0 ou 2 ? ');
 
-        }
-        /* obst = readlineSync.question('Sensor meio obstaculo 0 ou 2 ? ');
-        if (poluicao == 0 && obst == 0) {
-            Matriz_Mapeamento[i + 1][j] = 1;
-            Matriz_Mapeamento[i][j] = 1;
-            Matriz_Mapeamento[i - 1][j] = 1;
-        }
-        else if (obst == 2) {
-            Matriz_Mapeamento[i][j] = 2;
-            obst1 = readlineSync.question('Sensor cima obstaculo 0 ou 2 ? ');
-            if (obst1 == 2) {
-                Matriz_Mapeamento[i + 1][j] = 2;
-            }
-            else if (poluicao == 0) {
-                Matriz_Mapeamento[i + 1][j] = 1;
-            }
-            else
-                Matriz_Mapeamento[i + 1][j] = 3;
-            obst2 = readlineSync.question('Sensor baixo obstaculo 0 ou 2 ? ');
-            if (obst2 == 2) {
-                Matriz_Mapeamento[i - 1][j] = 2;
-            }
-            else if (poluicao == 0) {
-                Matriz_Mapeamento[i - 1][j] = 1;
-            }
-            else
-                Matriz_Mapeamento[i - 1][j] = 3;
-        }
-        else if (poluicao == 3) {
-            Matriz_Mapeamento[i + 1][j] = 3;
-            Matriz_Mapeamento[i][j] = 3;
-            Matriz_Mapeamento[i - 1][j] = 3;
-        }
-        else {
-            Matriz_Mapeamento[i][j] = 4;
-        } */
-        console.clear();
-        console.table(Matriz_Mapeamento);
-        ASV_Moves(i, j);
-        if ((j < 4) && (flag == 0))
+        //head = 'R' - Dir, 'L' - Esq, 'U' - Cim, 'D' - Bx
+        if ((head == 'R') && (j + 1 < Matriz_Mapeamento[i].length) && (Matriz_Mapeamento[i][j + 1] != 2) && (Matriz_Mapeamento[i][j + 1] != 4)) {
             j++;
-        else if ((j == 4) && (flag == 0)) {
-            i += 3;
-            flag = 1;
+            console.log("Caminho livre incremento a coluna", j);
+            ASV_Moves(i, j);
+            sensors(i, j, head);
+            console.table(Matriz_ASV);
+            console.table(Matriz_Mapeamento);
         }
-        else if ((j > 0) && (flag == 1)) {
+        else if ((head == 'R') && ((j + 1 >= Matriz_Mapeamento[i].length) || (Matriz_Mapeamento[i][j + 1] == 2 || Matriz_Mapeamento[i][j + 1] == 4))) {
+            head = 'D';
+            console.log("Caminho impedido mudei direção :", i, j, head);
+            /*             if (Matriz_Mapeamento[i+1][j] != 2 && Matriz_Mapeamento[i+1][j] == 4) {
+                            flag = 1;
+                            aux_i = i;
+                            for (aux_j = j; aux_j < Matriz_Mapeamento[i].length; aux_j++)
+                                if ((Matriz_Mapeamento[i][j + 1] != 2) && (Matriz_Mapeamento[i][j + 1] != 2))
+                                    break;
+            
+                        } */
+            //if (Matriz_Mapeamento[i + 1][j] != 1 && Matriz_Mapeamento[i + 1][j] != 1)
+            sensors(i, j, head);
+            console.table(Matriz_Mapeamento);
+        }
+        /********/
+        if ((head == 'D') && (Matriz_Mapeamento[i + 1][j] != 2) && (Matriz_Mapeamento[i + 1][j] != 4) && (i + 1 < Matriz_Mapeamento.length)) {
+            i++;
+            console.log("Caminho desimpedido incremento a linha :", i, j, head);
+            ASV_Moves(i, j);
+            sensors(i, j, head);
+            head = 'R';
+            console.table(Matriz_ASV);
+            console.table(Matriz_Mapeamento);
+        }
+        else if ((head == 'D') && ((Matriz_Mapeamento[i + 1][j] == 2) || (Matriz_Mapeamento[i + 1][j] == 4) || (i + 1 >= Matriz_Mapeamento.length))) {
+            head = 'U';
+            console.log("Caminho impedido mudei direção :", i, j, head);
+            sensors(i, j, head);
+            console.table(Matriz_Mapeamento);
+        }
+        /********/
+        if ((head == 'U') && (Matriz_Mapeamento[i - 1][j] != 2) && (Matriz_Mapeamento[i - 1][j] != 4) && (i - 1 >= 0)) {
+            i--;
+            console.log("Caminho livre decremento a linha", i);
+            ASV_Moves(i, j);
+            sensors(i, j, head);
+            head = 'R'
+            console.table(Matriz_ASV);
+            console.table(Matriz_Mapeamento);
+        }
+        else if ((head == 'U') && ((Matriz_Mapeamento[i - 1][j] == 2) || (Matriz_Mapeamento[i - 1][j] == 4) || (i - 1 < 0))) {
+            head = 'L';
+            console.log("Caminho impedido mudei direção :", i, j, head);
+            sensors(i, j, head);
+            console.table(Matriz_Mapeamento);
+        }
+        /***********/
+        if ((head == 'L') && (j - 1 >= 0) && (Matriz_Mapeamento[i][j - 1] != 2) && (Matriz_Mapeamento[i][j - 1] != 4)) {
             j--;
+            console.log("Caminho livre decremento a coluna", j);
+            ASV_Moves(i, j);
+            sensors(i, j, head);
+            head = 'D';
+            console.table(Matriz_ASV);
+            console.table(Matriz_Mapeamento);
         }
-        else if ((j == 0) && (flag == 1)) {
-            i += 3;
-            flag = 0;
-        }
-        if (i >= 11)
-            opt = 0;
-        else {
-            opt = readlineSync.question('Sair 0 ou 1 ?');
+        else if ((head == 'L') && ((j - 1 < 0) || (Matriz_Mapeamento[i][j - 1] == 2 || Matriz_Mapeamento[i][j - 1] == 4))) {
+            head = 'D';
+            console.log("Caminho impedido mudei direção :", i, j, head);
+            sensors(i, j, head);
+            console.table(Matriz_Mapeamento);
         }
     }
 }
